3959522816520a186697094a758b3a751d34219a
Sajid Mehmood Tariq <149877372+sajidmtariq30@users.noreply.github.com>
2024-02-09 11:23:11 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const time = document.getElementById("time");
+const timeformat = document.getElementById("timeformat");
+
+document.addEventListener("DOMContentLoaded", () => {
+    setInterval(showtime, 1000);
+});
+
+const showtime = () => {
+    let date = new Date();
+
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    hr = hr<10 ? `0${hr}` :hr; 
+    min = min<10 ? `0${min}` :min; 
+    sec = sec<10 ? `0${sec}` :sec; 
+    time.innerHTML = `${hr} : ${min} : ${sec} `
+    // console.log(hr + " hours " + min + " minutes " + sec + " seconds")
+
+    timeformat.innerHTML = hr>=12 ? "PM" : "AM";
+}
